38f05fbb9669a86cb632e0bc84cbcadcbbacc7cf
kanyanat999 <kanyanat_khueankham@cmu.ac.th>
2024-12-13 14:08:57 +0700

    lab8/finish

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,16 @@
 "Brook"
 "Jinbe"
 */
+char findGrade(double score) {
+    if (score > 90) {
+        return 'A';
+    } else if (score > 75) {
+        return 'B';
+    } else if (score > 60) {
+        return 'C';
+    } else if (score > 45) {
+        return 'D';
+    } else {
+        return 'F';
+    }
+}
